e55cb7fbb11c9d31276e65ea8b1bc0bc409bb258
fjtughy <98021263+fjtughy@users.noreply.github.com>
2022-06-02 18:15:32 +0900

    Allow more lineType to relation
    + LineType.DOT_DASHED,  
    + LineType.DOT_DOT_DASHED,  
    + LineType.DOUBLE, 
    +  LineType.DOUBLE_DASHED,  
    + LineType.DOUBLE_DOTTED

--- umlet-elements/src/main/java/com/baselet/element/relation/facet/RelationLineTypeFacet.java
+++ umlet-elements/src/main/java/com/baselet/element/relation/facet/RelationLineTypeFacet.java
@@ -53,7 +53,7 @@ public class RelationLineTypeFacet extends FirstRunKeyValueFacet {
 	private static final List<ArrowEnd> SHARED_ARROW_STRINGS_AFTER = Arrays.asList(ArrowEnd.DOUBLE_PIPE, ArrowEnd.SINGLE_PIPE, ArrowEnd.SMALL_CIRCLE, ArrowEnd.BOX);
 	private static final List<ArrowEnd> LEFT_ARROW_STRINGS = SharedUtils.mergeLists(SHARED_ARROW_STRINGS_BEFORE, Arrays.asList(ArrowEnd.LEFT_BOX, ArrowEnd.LEFT_FILLED_DIAMOND, ArrowEnd.LEFT_DIAMOND, ArrowEnd.LEFT_FILLED_CLOSED, ArrowEnd.LEFT_CLOSED, ArrowEnd.LEFT_NORMAL, ArrowEnd.LEFT_ZERO_TO_MANY, ArrowEnd.LEFT_ONE_TO_MANY, ArrowEnd.LEFT_INVERTED, ArrowEnd.LEFT_INTERFACE_OPEN, ArrowEnd.LEFT_MEASURE_NORMAL), SHARED_ARROW_STRINGS_AFTER);
 	private static final List<ArrowEnd> RIGHT_ARROW_STRINGS = SharedUtils.mergeLists(SHARED_ARROW_STRINGS_BEFORE, Arrays.asList(ArrowEnd.RIGHT_BOX, ArrowEnd.RIGHT_FILLED_DIAMOND, ArrowEnd.RIGHT_DIAMOND, ArrowEnd.RIGHT_FILLED_CLOSED, ArrowEnd.RIGHT_CLOSED, ArrowEnd.RIGHT_MEASURE_NORMAL, ArrowEnd.RIGHT_NORMAL, ArrowEnd.RIGHT_ZERO_TO_MANY, ArrowEnd.RIGHT_ONE_TO_MANY, ArrowEnd.RIGHT_INVERTED, ArrowEnd.RIGHT_INTERFACE_OPEN), SHARED_ARROW_STRINGS_AFTER);
-	private static final List<LineType> LINE_TYPES = Arrays.asList(LineType.SOLID, LineType.DOTTED, LineType.DASHED);
+	private static final List<LineType> LINE_TYPES = Arrays.asList(LineType.SOLID, LineType.DOTTED, LineType.DASHED,  LineType.DOT_DASHED,  LineType.DOT_DOT_DASHED,  LineType.DOUBLE,  LineType.DOUBLE_DASHED,  LineType.DOUBLE_DOTTED);
 
 	public RelationPointHandler getRelationPoints(PropertiesParserState state) {
 		return ((SettingsRelation) state.getSettings()).getRelationPoints();
